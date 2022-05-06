897558f811f1dd6c08005957e059bd78f4ad209a
Roohollah Mousavi <s.r.mousavi115@gmail.com>
2022-05-06 22:34:09 +0430

    feat: add html of user table

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,41 @@ function App() {
                     </div>
                 </div>
             </header>
+            <div className="container">
+                <section>
+                    <div className="d-flex justify-content-end mb-4">
+                        <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal">Add User</button>
+                    </div>
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First Name</th>
+                                <th scope="col">Last Name</th>
+                                <th scope="col">Is Admin</th>
+                                <th scope="col">Job Title</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Mobile</th>
+                                <th scope="col">Join Date</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row"></th>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+            </div>
         </>
     )
 }
