3359c78eef124182e8bff6ab69c85eacfa9e8cba
Roohollah Mousavi <s.r.mousavi115@gmail.com>
2022-05-06 22:33:01 +0430

    feat: add html of app header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 function App() {
-    return <></>
+    return (
+        <>
+            <header>
+                <div className="navbar navbar-dark bg-dark shadow-sm mb-4">
+                    <div className="container d-flex justify-content-between">
+                        <span className="navbar-brand">
+                            <strong>Users App</strong>
+                        </span>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
 }
 
 export default App
