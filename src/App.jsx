54070de63d8829b8970826bf1770c6fac5b0255e
Roohollah Mousavi <s.r.mousavi115@gmail.com>
2022-05-07 00:18:50 +0430

    feat: create UserList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import Header from "./Components/Header"
+import Header from './Components/Header'
+import UserList from './Components/UserList'
 
 function App() {
     return (
@@ -15,34 +16,7 @@ function App() {
                             Add User
                         </button>
                     </div>
-                    <table className="table table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">First Name</th>
-                                <th scope="col">Last Name</th>
-                                <th scope="col">Is Admin</th>
-                                <th scope="col">Job Title</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Mobile</th>
-                                <th scope="col">Join Date</th>
-                                <th scope="col"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row"></th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <UserList />
                 </div>
             </main>
             <div className="modal" tabIndex="-1" id="add-user-modal">
---
+++ src/Components/UserList.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function UserList() {
+    return (
+        <table className="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Is Admin</th>
+                    <th scope="col">Job Title</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Mobile</th>
+                    <th scope="col">Join Date</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row"></th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default UserList
