d8cff98b3b3d012e57d8fa5d47d9261cdcb681df
Roohollah Mousavi <s.r.mousavi115@gmail.com>
2022-05-16 21:58:50 +0430

    refactor: remove React.StrictMode

--- src/index.js
+++ src/index.js
@@ -5,8 +5,4 @@ import 'bootstrap/js/dist/modal'
 import App from './app'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<App />)
