e3a7a6aba1c6408e1f11b484875d1ec179e8de2e
fatemeh-pooladrag-developer <fa.pooladrag@gmail.com>
2022-05-16 18:44:57 +0430

    Add validator for validation some fields in adduserform

--- src/Components/addUserFormModal.jsx
+++ src/Components/addUserFormModal.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import { useUsersDispatch } from '../hooks/useUsersDispatch'
 import Input from './ui/input'
-import { Modal, Button } from 'react-bootstrap'
+import { Modal } from 'react-bootstrap'
+import validator from 'validator'
 
 function AddUserFormModal({ show, handleClose }) {
     const initailState = {
@@ -15,6 +16,7 @@ function AddUserFormModal({ show, handleClose }) {
         joinDate: '',
     }
     const [user, setUser] = useState(initailState)
+    const [error, setError] = useState('')
     const dispatch = useUsersDispatch()
 
     const handleInputChange = (e) => {
@@ -36,95 +38,119 @@ function AddUserFormModal({ show, handleClose }) {
         return now
     }
 
+    const checkValidation = () => {
+        if (validator.isEmpty(user.email) || validator.isEmpty(user.firstName)) {
+            setError('Email and First name are required.')
+            return false
+        } else if (!validator.isEmail(user.email)) {
+            setError('Email address is invalid')
+            return false
+        }
+
+        return true
+    }
+
     const handleSaveButtonClick = () => {
-        user.id = Date.now()
-        user.joinDate = getCurrentDate()
-        dispatch({
-            type: 'add_user',
-            payload: {
-                user,
-            },
-        })
+        if (checkValidation()) {
+            user.id = Date.now()
+            user.joinDate = getCurrentDate()
+            dispatch({
+                type: 'add_user',
+                payload: {
+                    user,
+                },
+            })
+            close()
+        }
+    }
+
+    const save = () => {}
+
+    const close = () => {
+        setError('')
         setUser(initailState)
         handleClose()
     }
 
     return (
-        <Modal show={show} onHide={handleClose}>
+        <Modal show={show} onHide={close}>
             <Modal.Header closeButton>
                 <Modal.Title>Add User Form</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <form>
-                    <div className="row">
-                        <div className="mb-3 col-12 col-md-6">
-                            <Input
-                                type="text"
-                                id="first-name"
-                                name="firstName"
-                                value={user.firstName}
-                                onChange={handleInputChange}
-                                label="First name"
-                            />
-                        </div>
-                        <div className="mb-3 col-12 col-md-6">
-                            <Input
-                                type="text"
-                                id="last-name"
-                                name="lastName"
-                                value={user.lastName}
-                                onChange={handleInputChange}
-                                label="Last name"
-                            />
-                        </div>
-
-                        <div className="mb-3 col-12 col-md-6">
-                            <Input
-                                type="text"
-                                id="job-title"
-                                name="jobTitle"
-                                value={user.jobTitle}
-                                onChange={handleInputChange}
-                                label="Job title"
-                            />
-                        </div>
-                        <div className="mb-3 col-12 col-md-6">
-                            <Input
-                                type="text"
-                                id="email"
-                                name="email"
-                                value={user.email}
-                                onChange={handleInputChange}
-                                label="Email"
-                            />
-                        </div>
-                        <div className="mb-3 col-12 col-md-6">
-                            <Input
-                                type="text"
-                                id="mobile"
-                                name="mobile"
-                                value={user.mobile}
-                                onChange={handleInputChange}
-                                label="Mobile"
-                            />
-                        </div>
-                        <div className="mb-3 col-12 col-md-6 d-flex align-items-center">
-                            <div className="form-check">
+                <>
+                    {error && <h6 className="text-danger">{error}</h6>}
+                    <form>
+                        <div className="row">
+                            <div className="mb-3 col-12 col-md-6">
                                 <Input
-                                    type="checkbox"
-                                    id="is-admin"
-                                    name="isAdmin"
-                                    checked={user.isAdmin}
+                                    type="text"
+                                    id="first-name"
+                                    name="firstName"
+                                    value={user.firstName}
                                     onChange={handleInputChange}
-                                    label="Is admin"
+                                    label="First name"
                                 />
                             </div>
+                            <div className="mb-3 col-12 col-md-6">
+                                <Input
+                                    type="text"
+                                    id="last-name"
+                                    name="lastName"
+                                    value={user.lastName}
+                                    onChange={handleInputChange}
+                                    label="Last name"
+                                />
+                            </div>
+
+                            <div className="mb-3 col-12 col-md-6">
+                                <Input
+                                    type="text"
+                                    id="job-title"
+                                    name="jobTitle"
+                                    value={user.jobTitle}
+                                    onChange={handleInputChange}
+                                    label="Job title"
+                                />
+                            </div>
+                            <div className="mb-3 col-12 col-md-6">
+                                <Input
+                                    type="text"
+                                    id="email"
+                                    name="email"
+                                    value={user.email}
+                                    onChange={handleInputChange}
+                                    label="Email"
+                                />
+                            </div>
+                            <div className="mb-3 col-12 col-md-6">
+                                <Input
+                                    type="text"
+                                    id="mobile"
+                                    name="mobile"
+                                    value={user.mobile}
+                                    onChange={handleInputChange}
+                                    label="Mobile"
+                                />
+                            </div>
+                            <div className="mb-3 col-12 col-md-6 d-flex align-items-center">
+                                <div className="form-check">
+                                    <Input
+                                        type="checkbox"
+                                        id="is-admin"
+                                        name="isAdmin"
+                                        checked={user.isAdmin}
+                                        onChange={handleInputChange}
+                                        label="Is admin"
+                                    />
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </>
             </Modal.Body>
             <Modal.Footer>
-                <button type="button" className="btn btn-secondary" onClick={handleClose}>
+                <button type="button" className="btn btn-secondary" onClick={close}>
                     Close
                 </button>
                 <button type="button" className="btn btn-primary" onClick={handleSaveButtonClick}>
